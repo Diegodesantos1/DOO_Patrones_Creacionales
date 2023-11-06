a4e77696ef538c6cfdb5c7a42842462a53c48310
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-07 00:26:23 +0100

    Ahora sí, por fin Django creado correctamente

--- PizzeriaWeb/PizzeriaWeb/asgi.py
+++ PizzeriaWeb/PizzeriaWeb/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for Proyecto_Pizzería project.
+ASGI config for PizzeriaWeb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Proyecto_Pizzería.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PizzeriaWeb.settings")
 
 application = get_asgi_application()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 eleccion =int(input("¿Qué ejercicio quieres ejecutar?\n1 --> Emergencias\n2 --> Pizzeria\n"))
 
 if eleccion == 1:
@@ -7,7 +8,7 @@ if eleccion == 1:
         client_code(FactoryEstadisticas())
 
 elif eleccion == 2:
-    eleccion2 = int(input("¿Qué ejercicio quieres ejecutar?\n1 --> Terminal\n2 --> Interfaz Gráfica\n"))
+    eleccion2 = int(input("¿Qué ejercicio quieres ejecutar?\n1 --> Terminal\n2 --> Interfaz Gráfica\n3 --> Django\n"))
 
     if eleccion2 == 1:
         from Pizzeria.main import *
@@ -18,3 +19,7 @@ elif eleccion == 2:
         if __name__ == "__main__":
             import Pizzeria.interfaz
             Pizzeria.interfaz.main()
+
+    elif eleccion2 == 3:
+        os.system("cd PizzeriaWeb")
+        os.system("python manage.py runserver")
